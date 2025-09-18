15b728c621bef8d19ce0f44f6154c44ad1f860a6
gaspiralli97 <dusmuratovismoil@gmail.com>
2025-09-18 08:02:58 +0500

    feat: build express web server

---
+++ server.js
@@ -0,0 +1,30 @@
+console.log(" Web Serverni boshlash");
+const express =require("express");
+const app = express();
+const http = require("http");
+
+// 1 Kirish
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+//2 Session
+
+//3 Views
+app.set("views","views");
+app.set("view engine","ejs");
+
+//4 Routing codesss
+app.get("/", function (req, res){
+    res.end("<h1>Hello World Everyone</h1>");
+});
+
+app.get("/gift", function (req, res){
+    res.end("<h1>you are in gift page</h1>");
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function() {
+    console.log(`the server is running successfully on port: ${PORT}`);
+});
